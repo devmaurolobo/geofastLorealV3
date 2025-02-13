de1d3f2bfffb276cf5520018f063510455ed870c
mauro2040 <105966365+mauro2040@users.noreply.github.com>
2025-02-13 04:47:44 -0300

    fix: correções gerais
    - Corrigido webhook URL em pages/api/videos.ts
    - Removido import não utilizado em SettingsPanel.tsx
    - Ajustado ProductCarousel/ProductCarrousel
    - Atualizado estilos do Header
    - Corrigido tipagem dos componentes

--- components/SettingsPanel.tsx
+++ components/SettingsPanel.tsx
@@ -7,7 +7,7 @@ import Slider from 'react-slick';
 import { Header } from './Header';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Product } from './types';
+
 
 
 // Adicionar interface SettingsPanelProps
